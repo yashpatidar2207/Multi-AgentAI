5738c22ab29893a7758ccf2a0e46640bb0417b0f
yashpatidar2207 <yashpatidar868@gmail.com>
2026-07-18 17:11:13 +0530

    connect auth service through gateway

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import proxy from "express-http-proxy";
 
 dotenv.config();
 
 const port = process.env.PORT
 const app=express()
 
+app.use("/auth",proxy(process.env.AUTH_SERVICE))
 app.get('/',(req,res)=>{
     return res.json({message:'msg gateway se'})
 })
